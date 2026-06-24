a7af7793bf52b5b922083c61862c6ed89d7900aa
sayan <sayan.nitd@gmail.com>
2026-06-24 15:09:11 +0200

    tested on rawhash

--- scripts/rh_zymo.sh
+++ scripts/rh_zymo.sh
@@ -5,17 +5,26 @@
 export ALL_TASKS="0-1"
 
 # other variables
-export DATADIR=$EXPDIR/data/simulated/Zymo
-export RAWHASH_DIR=~/Repos/RawHash/
-export RAWHASH=$RAWHASH_DIR/rawhash2
+export DATADIR=/data/SimulatedDatasets/Zymo
+export REF=$DATADIR/Refs1.fasta
+export SIGS=$DATADIR/PromethION_R10.4.1-seq2squiggle/signals/
+export RAWHASH_DIR=$CODEDIR/rawhash2
 
 f0() {
-    measure $RAWHASH -t 32 -x viral -d $TMPDIR/zymo.ind -p \
-        $RAWHASH_DIR/extern/kmer_models/legacy/legacy_r9.4_180mv_450bps_6mer/template_median68pA.model \
+    measure rawhash2 --r10 -t 32 -d $TMPDIR/zymo.ind -p \
+        $RAWHASH_DIR/extern/local_kmer_models/uncalled_r1041_model_only_means.txt \
         $DATADIR/Refs1.fasta
 }
 
 f1() {
-    measure $RAWHASH -x viral $TMPDIR/zymo.ind $DATADIR/signals/Sigs0_180.blow5 > $OUTDIR/zymo-rh.paf
-    measure $RAWHASH -x viral $TMPDIR/zymo.ind $DATADIR/signals/Sigs1_180.blow5 >> $OUTDIR/zymo-rh.paf
+    benchmark_aligner.py \
+        --input $SIGS/ \
+        --plugin pyrawhash \
+        --plugin-args "idx=$TMPDIR/zymo.ind threads=16 x=viral" \
+        --output $RESDIR/rawhash_zymo/
+}
+
+f2() {
+    measure rawhash2 -x viral $TMPDIR/zymo.ind $SIGS/Sigs0_1000.blow5 > $OUTDIR/zymo-rh.paf
+    measure rawhash2 -x viral $TMPDIR/zymo.ind $SIGS/Sigs1_1000.blow5 >> $OUTDIR/zymo-rh.paf
 }
