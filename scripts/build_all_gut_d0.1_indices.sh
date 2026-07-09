865c4f4a067a2373773c90db9e87fb39b6635fc8
sayan <sayan.nitd@gmail.com>
2026-07-09 23:15:47 +0200

    minor enhancements

---
+++ scripts/build_all_gut_d0.1_indices.sh
@@ -0,0 +1,77 @@
+#!/bin/bash
+
+# --- List of all tasks to run by default ---
+# This variable is used by the main script when no numbers are specified.
+export ALL_TASKS="1-7"
+
+# other variables
+export DATADIR=/data/SimulatedDatasets/Gut/
+export RAWHASH_DIR=$CODEDIR/rawhash2
+export OUTDIR=$TMPDIR/gutd1
+
+f1() {
+    mkdir -p $OUTDIR/cl
+    echo "Building Collinearity index for Gut (d=0.1).."
+    measure Collinearity \
+    --ref $DATADIR/Refs_d0.1_Comm_1.fa \
+    --idx $OUTDIR/cl/gutd0.1 \
+    --bw 1024
+}
+
+f2() {
+    mkdir -p $OUTDIR/sp
+    echo "Building Spumoni index for Gut (d=0.1).."
+    measure spumoni build -r $DATADIR/Refs_d0.1_Comm_1.fa -M -P -m -o $OUTDIR/sp/gutd0.1
+}
+
+f3() {
+    mkdir -p $OUTDIR/mg
+    echo "Building Metagraph index for Gut (d=0.1).."
+    #  metagraph build and annotate
+    measure metagraph build -v -p 16 -k 31 -o $OUTDIR/mg/gutd0.1 $DATADIR/Refs_d0.1_Comm_1.fa
+    measure metagraph annotate -v -p 16 -i $OUTDIR/mg/gutd0.1.dbg --anno-header -o $OUTDIR/mg/gutd0.1 $DATADIR/Refs_d0.1_Comm_1.fa 
+}
+
+f4() {
+    mkdir -p $OUTDIR/mm
+    echo "Building Minimap2 index for Gut (d=0.1).."
+    measure minimap2 -x map-ont -d $OUTDIR/mm/gutd0.1.mmi $DATADIR/Refs_d0.1_Comm_1.fa
+}
+
+f5() {
+    mkdir -p $OUTDIR/rb
+    echo "Building readBouncer index for Gut (d=0.1).."
+    local config_file
+    config_file="$(mktemp --suffix=.toml)"
+
+    # cleanup fires when this function returns, however it returns
+    trap 'rm -f "$config_file"' RETURN
+
+    cat > "$config_file" <<EOF
+usage               = "build"
+output_directory    = "$OUTDIR/rb/gutd0.1"
+log_directory       = "$LOGDIR"
+
+[IBF]
+threads       = 8
+target_files  = ["$DATADIR/Refs_d0.1_Comm_1.fa"]
+EOF
+
+    measure ReadBouncer --config "$config_file"
+}
+
+f6() {
+    mkdir -p $OUTDIR/rh
+    echo "Building RawHash index for Gut (d=0.1).."
+    measure rawhash2 --r10 -t 32 -d $OUTDIR/rh/gutd0.1.ind -p \
+        $RAWHASH_DIR/extern/local_kmer_models/uncalled_r1041_model_only_means.txt \
+        $DATADIR/Refs_d0.1_Comm_1.fa
+}
+
+f7() {
+    mkdir -p $OUTDIR/sg
+    echo "Building Sigmoni index for Gut (d=0.1).."
+    measure sigmoni-index -p $DATADIR/Refs_d0.1_Comm_1.fa \
+        -o $OUTDIR/sg --spumoni-path $SPUMONI_BUILD_DIR \
+        --poremodel $CODEDIR/sigmoni/poremodel/model_r1041_400bps_dm_it2.tsv
+}
--- scripts/build_all_zymo_indices.sh
+++ scripts/build_all_zymo_indices.sh
@@ -7,32 +7,38 @@ export ALL_TASKS="1-7"
 # other variables
 export DATADIR=/data/SimulatedDatasets/Zymo
 export RAWHASH_DIR=$CODEDIR/rawhash2
+export OUTDIR=$TMPDIR/zymo
 
 f1() {
+    mkdir -p $OUTDIR/cl
     echo "Building Collinearity index for Zymo.."
     measure Collinearity \
     --ref $DATADIR/Refs1.fasta \
-    --idx $TMPDIR/Zymo
+    --idx $OUTDIR/cl/zymo
 }
 
 f2() {
+    mkdir -p $OUTDIR/sp
     echo "Building Spumoni index for Zymo.."
-    measure spumoni build -r $DATADIR/Refs1.fasta -M -P -m -o $TMPDIR/zymo
+    measure spumoni build -r $DATADIR/Refs1.fasta -M -P -m -o $OUTDIR/sp/zymo
 }
 
 f3() {
+    mkdir -p $OUTDIR/mg
     echo "Building Metagraph index for Zymo.."
     #  metagraph build and annotate
-    measure metagraph build -v -p 16 -k 31 -o $TMPDIR/mg_zymo $DATADIR/Refs1.fasta
-    measure metagraph annotate -v -p 16 -i $TMPDIR/mg_zymo.dbg --anno-header -o $TMPDIR/mg_zymo $DATADIR/Refs1.fasta 
+    measure metagraph build -v -p 16 -k 31 -o $OUTDIR/mg/zymo $DATADIR/Refs1.fasta
+    measure metagraph annotate -v -p 16 -i $OUTDIR/mg/zymo.dbg --anno-header -o $OUTDIR/mg/zymo $DATADIR/Refs1.fasta 
 }
 
 f4() {
+    mkdir -p $OUTDIR/mm
     echo "Building Minimap2 index for Zymo.."
-    measure minimap2 -x map-ont -d $TMPDIR/zymo.mmi $DATADIR/Refs1.fasta
+    measure minimap2 -x map-ont -d $OUTDIR/mm/zymo.mmi $DATADIR/Refs1.fasta
 }
 
 f5() {
+    mkdir -p $OUTDIR/rb
     echo "Building readBouncer index for Zymo.."
     local config_file
     config_file="$(mktemp --suffix=.toml)"
@@ -42,7 +48,7 @@ f5() {
 
     cat > "$config_file" <<EOF
 usage               = "build"
-output_directory    = "$TMPDIR"
+output_directory    = "$OUTDIR/rb/zymo"
 log_directory       = "$LOGDIR"
 
 [IBF]
@@ -54,15 +60,17 @@ EOF
 }
 
 f6() {
+    mkdir -p $OUTDIR/rh
     echo "Building RawHash index for Zymo.."
-    measure rawhash2 --r10 -t 32 -d $TMPDIR/zymo.ind -p \
+    measure rawhash2 --r10 -t 32 -d $OUTDIR/rh/zymo.ind -p \
         $RAWHASH_DIR/extern/local_kmer_models/uncalled_r1041_model_only_means.txt \
         $DATADIR/Refs1.fasta
 }
 
 f7() {
+    mkdir -p $OUTDIR/sg
     echo "Building Sigmoni index for Zymo.."
     measure sigmoni-index -p $DATADIR/Refs1.fasta \
-        -o $TMPDIR --spumoni-path $SPUMONI_BUILD_DIR \
+        -o $OUTDIR/sg --spumoni-path $SPUMONI_BUILD_DIR \
         --poremodel $CODEDIR/sigmoni/poremodel/model_r1041_400bps_dm_it2.tsv
 }
