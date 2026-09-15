e1a3c8c78d1c5addd1cd0dfb1028833b09bcdce3
sayan <sayan.nitd@gmail.com>
2026-09-15 17:08:12 +0200

    Add multi-threading support and enhance index building scripts for Gut and Zymo

--- scripts/build_all_gut_d0.1_indices.sh
+++ scripts/build_all_gut_d0.1_indices.sh
@@ -9,37 +9,44 @@ export DATADIR=/data/SimulatedDatasets/Gut/
 export RAWHASH_DIR=$CODEDIR/rawhash2
 export OUTDIR=$TMPDIR/gutd1
 export REF=$DATADIR/Refs_d0.1_Comm_1.fa
+export NUM_THREADS=32
+export OMP_NUM_THREADS=$NUM_THREADS
 
 f1() {
+    sleep 2
     mkdir -p $OUTDIR/cl
     echo "Building Collinearity index for Gut (d=0.1).."
     measure Collinearity \
     --ref $REF \
     --idx $OUTDIR/cl/gutd0.1 \
-    --bw 1024
+    --bw 1024 --n_threads $NUM_THREADS
 }
 
 f2() {
+    sleep 2
     mkdir -p $OUTDIR/sp
     echo "Building Spumoni index for Gut (d=0.1).."
     measure spumoni build -r $REF -M -P -m -o $OUTDIR/sp/gutd0.1
 }
 
 f3() {
+    sleep 2
     mkdir -p $OUTDIR/mg
     echo "Building Metagraph index for Gut (d=0.1).."
     #  metagraph build and annotate
-    measure metagraph build -v -p 16 -k 21 -o $OUTDIR/mg/gutd0.1 $REF
-    measure metagraph annotate -v -p 16 -i $OUTDIR/mg/gutd0.1.dbg --anno-header -o $OUTDIR/mg/gutd0.1 $REF 
+    measure metagraph build --mode canonical -v -p $NUM_THREADS -k 19 -o $OUTDIR/mg/gutd0.1 $REF
+    measure metagraph annotate -v -p $NUM_THREADS -i $OUTDIR/mg/gutd0.1.dbg --anno-label "found" -o $OUTDIR/mg/gutd0.1 $REF 
 }
 
 f4() {
+    sleep 2
     mkdir -p $OUTDIR/mm
     echo "Building Minimap2 index for Gut (d=0.1).."
-    measure minimap2 -x map-ont -d $OUTDIR/mm/gutd0.1.mmi $REF
+    measure minimap2 -t $NUM_THREADS -x map-ont -d $OUTDIR/mm/gutd0.1.mmi $REF
 }
 
 f5() {
+    sleep 2
     mkdir -p $OUTDIR/rb
     echo "Building readBouncer index for Gut (d=0.1).."
     local config_file
@@ -54,7 +61,8 @@ output_directory    = "$OUTDIR/rb"
 log_directory       = "$LOGDIR"
 
 [IBF]
-threads       = 8
+kmer_size     = 17
+threads       = $NUM_THREADS
 target_files  = ["$REF"]
 EOF
 
@@ -62,14 +70,16 @@ EOF
 }
 
 f6() {
+    sleep 2
     mkdir -p $OUTDIR/rh
     echo "Building RawHash index for Gut (d=0.1).."
-    measure rawhash2 --r10 -t 32 -d $OUTDIR/rh/gutd0.1.ind -p \
+    measure rawhash2 --r10 -t $NUM_THREADS -d $OUTDIR/rh/gutd0.1.ind -p \
         $RAWHASH_DIR/extern/local_kmer_models/uncalled_r1041_model_only_means.txt \
         $REF
 }
 
 f7() {
+    sleep 2
     mkdir -p $OUTDIR/sg
     echo "Building Sigmoni index for Gut (d=0.1).."
     measure sigmoni-index -p $REF \
--- scripts/build_all_zymo_indices.sh
+++ scripts/build_all_zymo_indices.sh
@@ -8,37 +8,44 @@ export ALL_TASKS="1-7"
 export DATADIR=/data/SimulatedDatasets/Zymo
 export RAWHASH_DIR=$CODEDIR/rawhash2
 export OUTDIR=$TMPDIR/zymo
+export NUM_THREADS=32
+export OMP_NUM_THREADS=$NUM_THREADS
 
 f1() {
+    sleep 2
     mkdir -p $OUTDIR/cl
     echo "Building Collinearity index for Zymo.."
     measure Collinearity \
     --ref $DATADIR/Refs1.fasta \
     --idx $OUTDIR/cl/zymo \
-    --bw 1024
+    --bw 1024 --sparse --n_threads $NUM_THREADS
 }
 
 f2() {
+    sleep 2
     mkdir -p $OUTDIR/sp
     echo "Building Spumoni index for Zymo.."
     measure spumoni build -r $DATADIR/Refs1.fasta -M -P -m -o $OUTDIR/sp/zymo
 }
 
 f3() {
+    sleep 2
     mkdir -p $OUTDIR/mg
     echo "Building Metagraph index for Zymo.."
     #  metagraph build and annotate
-    measure metagraph build -v -p 16 -k 21 -o $OUTDIR/mg/zymo $DATADIR/Refs1.fasta
-    measure metagraph annotate -v -p 16 -i $OUTDIR/mg/zymo.dbg --anno-header -o $OUTDIR/mg/zymo $DATADIR/Refs1.fasta 
+    measure metagraph build --mode canonical -v -p $NUM_THREADS -k 19 -o $OUTDIR/mg/zymo $DATADIR/Refs1.fasta
+    measure metagraph annotate -v -p $NUM_THREADS -i $OUTDIR/mg/zymo.dbg --anno-header -o $OUTDIR/mg/zymo $DATADIR/Refs1.fasta 
 }
 
 f4() {
+    sleep 2
     mkdir -p $OUTDIR/mm
     echo "Building Minimap2 index for Zymo.."
-    measure minimap2 -x map-ont -d $OUTDIR/mm/zymo.mmi $DATADIR/Refs1.fasta
+    measure minimap2 -t $NUM_THREADS -x map-ont -d $OUTDIR/mm/zymo.mmi $DATADIR/Refs1.fasta
 }
 
 f5() {
+    sleep 2
     mkdir -p $OUTDIR/rb
     echo "Building readBouncer index for Zymo.."
     local config_file
@@ -53,7 +60,8 @@ output_directory    = "$OUTDIR/rb"
 log_directory       = "$LOGDIR"
 
 [IBF]
-threads       = 8
+kmer_size     = 17
+threads       = $NUM_THREADS
 target_files  = ["$DATADIR/Refs1.fasta"]
 EOF
 
@@ -61,14 +69,16 @@ EOF
 }
 
 f6() {
+    sleep 2
     mkdir -p $OUTDIR/rh
     echo "Building RawHash index for Zymo.."
-    measure rawhash2 --r10 -t 32 -d $OUTDIR/rh/zymo.ind -p \
+    measure rawhash2 --r10 -t $NUM_THREADS -d $OUTDIR/rh/zymo.ind -p \
         $RAWHASH_DIR/extern/local_kmer_models/uncalled_r1041_model_only_means.txt \
         $DATADIR/Refs1.fasta
 }
 
 f7() {
+    sleep 2
     mkdir -p $OUTDIR/sg
     echo "Building Sigmoni index for Zymo.."
     measure sigmoni-index -p $DATADIR/Refs1.fasta \
